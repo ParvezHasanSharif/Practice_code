05d06911791c045bccef1f5a17f338fb2af6dedb
ParvezHasanSharif <sharifislamabc@gmail.com>
2025-08-08 00:34:41 +0600

    wrote some code

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // Includes the input/output stream library
+
+// Using the standard namespace to avoid prefixing standard library elements with 'std::'
+using namespace std; 
+
+// The main function where program execution begins
+int main() {
+    // Prints "Hello World!" to the console
+    cout << "Hello World!" << endl; 
+    
+    // Returns 0 to indicate successful program execution
+    return 0; 
+}
